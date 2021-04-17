8020708555eb89e21769a9607809b08faa71aa56
Cleilson <marcialwushu@gmail.com>
2021-04-17 01:44:08 -0300

    Create Product.cs

---
+++ lang/csharp/Facade Design Pattern in C#/Product.cs
@@ -0,0 +1,11 @@
+using System;
+namespace FacadeDesignPattern
+{
+    public class Product
+    {
+        public void GetProductDetails()
+        {
+            Console.WriteLine("Fetching the Product Details");
+        }
+    }
+}
