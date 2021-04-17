2cc27d92636e0d5617f14120a53233e48c262752
Cleilson <marcialwushu@gmail.com>
2021-04-17 01:46:49 -0300

    Create Program.cs

---
+++ lang/csharp/Facade Design Pattern in C#/Client/Program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace FacadeDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
+        }
+    }
+}
